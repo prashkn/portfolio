dd5c254fc66a3fad91965024b2e08a19514644d7
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-25 13:45:10 -0500

    include only navigation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./modules/Navigation";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
