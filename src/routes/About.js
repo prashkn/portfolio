8592394a20710386cc3f1868197d54264d446b35
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-25 13:46:51 -0500

    skeleton template created

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function About() {
+  return (
+    <Container>
+      <Col>
+        <Row>hi</Row>
+      </Col>
+    </Container>
+  );
+}
---
+++ src/routes/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Contact() {
+  return <div>contact</div>;
+}
